1438b25b070e66fadc1f1d634f950a8af574edc6
eldervaz <eldervaz@gmail.com>
2016-05-25 10:09:17 -0500

    first update from Unreal

---
+++ Source/Test2Unreal/Test2Unreal.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Test2Unreal.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Test2Unreal, "Test2Unreal" );
---
+++ Source/Test2Unreal/Test2UnrealGameMode.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Test2Unreal.h"
+#include "Test2UnrealGameMode.h"
+
+
+
+
